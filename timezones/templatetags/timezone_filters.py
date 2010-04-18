a3a9d435635346afb2852ccc05004c88701dec02
Kyle MacFarlane <kyle@deletethetrees.com>
2010-04-18 20:44:17 +0100

    Added from_localtime filter to convert to settings.TIME_ZONE

--- timezones/templatetags/timezone_filters.py
+++ timezones/templatetags/timezone_filters.py
@@ -1,8 +1,6 @@
-
-from django.template import Node
 from django.template import Library
 
-from timezones.utils import localtime_for_timezone
+from timezones.utils import adjust_datetime_to_timezone, localtime_for_timezone
 
 register = Library()
 
@@ -10,3 +8,6 @@ def localtime(value, timezone):
     return localtime_for_timezone(value, timezone)
 register.filter("localtime", localtime)
 
+def from_localtime(value, timezone):
+    return adjust_datetime_to_timezone(value, timezone)
+register.filter("from_localtime", from_localtime)
